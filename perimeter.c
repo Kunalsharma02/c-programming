b3731ea04393374e707a88d6807211f433dbb433
Kunalsharma02 <92158197+Kunalsharma02@users.noreply.github.com>
2021-10-12 22:31:40 +0530

    Add files via upload

---
+++ perimeter.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    float length, width, perimeter;
+    printf("Enter length of the rectangle: ");
+    scanf("%f", &length);
+    printf("Enter width of the rectangle: ");
+    scanf("%f", &width);
+    perimeter = 2 * (length + width);
+    printf("Perimeter of rectangle = %f ", perimeter);
+    return 0;
+}
+© 2021 GitHub
