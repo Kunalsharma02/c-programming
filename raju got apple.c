e8800cd8173ada53fe97461486475ee127f8b34f
Kunalsharma02 <92158197+Kunalsharma02@users.noreply.github.com>
2021-10-14 23:59:05 +0530

    Add files via upload

---
+++ raju got apple.c
@@ -0,0 +1,20 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+   float n,tn;
+   n=6.5;
+   tn=3*6.5;
+   printf("the total number of apples raju got are\n");
+   printf("%f",tn);
+    return 0;
+}
+
