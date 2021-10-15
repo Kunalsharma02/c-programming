13f7c51bffaabf5dcc59289c0bc5b7cab6a06f5d
Kunalsharma02 <92158197+Kunalsharma02@users.noreply.github.com>
2021-10-15 09:48:29 +0530

    Add files via upload

---
+++ populatin ater 2 years.c
@@ -0,0 +1,24 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+ int p,a,b,tp,pa;
+ p=30000;
+ a=(20*p)/100;
+ pa=p+a;
+ b=(30*pa)/100;
+ tp=p+a+b;
+ printf("the total population after 2 years is");
+ printf("%d",tp);
+    
+    return 0;
+}
+
