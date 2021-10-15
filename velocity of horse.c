88409874ee90e2e961a5b70937cd3ad0081cc2b0
Kunalsharma02 <92158197+Kunalsharma02@users.noreply.github.com>
2021-10-15 10:30:16 +0530

    Add files via upload

---
+++ velocity of horse.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include<math.h>
+
+int main()
+{
+ int a,t,u,s,v;
+ a=4;
+ u=0;
+ t=3;
+ v=u+a*t;
+ printf("The final velocity of horse is \n");
+ printf("%d",v);
+ s=u*t+0.5*a*t*t;
+ printf("The distance travelled by the horse is \n");
+ printf("%d",s);
+ return 0;
+}
+
+
