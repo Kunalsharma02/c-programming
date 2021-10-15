74e680e395e2b52e621966ce62086f747552866c
Kunalsharma02 <92158197+Kunalsharma02@users.noreply.github.com>
2021-10-15 10:23:25 +0530

    Add files via upload

---
+++ velocity.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include<math.h>
+
+int main()
+{
+ int u,a,s,v, fv;
+ u=30;
+ a=5;
+ s=70;
+ v=u*u+2*a*s;
+ fv=sqrt(v);
+ printf("The final velocity of the car\n");
+ printf("%d",fv);
+ return 0;
+}
+
